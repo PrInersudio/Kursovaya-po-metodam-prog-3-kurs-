a4744a1dc27d635ddda9c8dfabd5f11a6fbd3037
PrInersudio <93220214+PrInersudio@users.noreply.github.com>
2022-12-18 16:54:47 +0300

    Добавлено потерянное закрытие формы

--- INN_checker.php
+++ INN_checker.php
@@ -14,7 +14,7 @@
             <p style="margin-left: 50px; font-size: 250%; line-height: 0%;"><label for="name">Enter IIN (first six or eight digits of bank card number)<br></label></p>
             <input style="margin-left: 100px; font-size: 200%; width: 152px;" name="IIN" type="text" maxlength="8" class="input-number" placeholder="00000000"/>
             <input style="margin-left: 20px; font-size: 200%;" type="submit" value="Check"/>
-            
+        </form>
             <p style="font-size: 200%;" align = "right">
                 Some IINs in database<br>
                 <?php
